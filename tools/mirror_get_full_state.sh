b50e65678d7d32e19201dab781d4e48682b13ff8
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2012-07-03 23:32:59 +0200

    Add message bus in call

--- tools/mirror_get_full_state.sh
+++ tools/mirror_get_full_state.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+#BUS=--session
+BUS=--system
 # Get state of the mirror
 #
 # Example result:
 # method return sender=:1.5 -> dest=:1.14 reply_serial=2
 #   string "UP"
-dbus-send --print-reply --dest=org.rfid.Mirror /org/rfid/mirror org.rfid.Mirror.getState
+dbus-send $BUS --print-reply --dest=org.rfid.Mirror /org/rfid/mirror org.rfid.Mirror.getState
 
 # Get list of tags on the mirror
 #
@@ -15,4 +17,4 @@ dbus-send --print-reply --dest=org.rfid.Mirror /org/rfid/mirror org.rfid.Mirror.
 #       string "D0021A0352C102B0"
 #       string "D0021A0352D003E1"
 #    ]
-dbus-send --print-reply --dest=org.rfid.Mirror /org/rfid/mirror org.rfid.Mirror.getTags
+dbus-send $BUS --print-reply --dest=org.rfid.Mirror /org/rfid/mirror org.rfid.Mirror.getTags
