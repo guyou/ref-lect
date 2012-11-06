614297c71474a801781aa6e9032d4c6a858cd877
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2012-11-06 21:37:41 +0100

    Add a test tool for the "wait" feature

---
+++ pam_ref-lect/wait_token.c
@@ -0,0 +1,17 @@
+#include "mirror.h"
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 2) {
+		fprintf (stderr, "Usage: %s delay tags...\n", argv[0]);
+		return 1;
+	}
+	int delay = atoi (argv[1]);
+	int i;
+	for (i = 2 ; i < argc ; i++) {
+		printf ("check returns %d\n", wait_token (argv[i], delay));
+	}
+	return 0;
+}
